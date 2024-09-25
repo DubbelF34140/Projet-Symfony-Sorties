d01e59361dd66e6c648f237711d57277b6360f69
DubbelF34140 <yw34140@gmail.com>
2024-09-25 09:28:22 +0200

    edit campus and ville controller

--- src/Controller/VilleController.php
+++ src/Controller/VilleController.php
@@ -9,88 +9,55 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class VilleController extends AbstractController
 {
-    #[Route('/admin/ville', name: 'app_ville')]
-    public function index(Request $request, EntityManagerInterface $entityManager): Response
+    #[Route('/admin/ville', name: 'app_ville', methods: ['GET', 'POST'])]
+    public function index(Request $request, VilleRepository $villeRepository, EntityManagerInterface $entityManager): Response
     {
-
         $ville = new Ville();
         $form = $this->createForm(VilleType::class, $ville);
-
         $form->handleRequest($request);
-        dump($ville);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->persist($ville);
             $entityManager->flush();
-            return $this->redirectToRoute('app_ville', ['up' => false]);
+
+            return $this->redirectToRoute('app_ville');
         }
 
-        $villes = $entityManager->getRepository(Ville::class)->findAll();
-
-        return $this->render('ville/index.html.twig', [
-            'villes' => $villes,
+        return $this->render('ville/admin.html.twig', [
+            'villes' => $villeRepository->findAll(),
             'form' => $form->createView(),
-            'up' => false
         ]);
     }
 
-//    #[Route('/admin/ville/create', name: 'app_admin_ville_create', methods: ['POST'])]
-//    public function create(Request $request,EntityManagerInterface $entityManager): Response
-//    {
-//        dump($request);
-//        //$ville = new Ville();
-//        $form = $this->createForm(VilleType::class, $ville);
-//
-//        $form->handleRequest($request);
-//        dump($ville);
-//
-//        if ($form->isSubmitted() && $form->isValid()) {
-//            $entityManager->persist($ville);
-//            $entityManager->flush();
-//            return $this->redirectToRoute('app_ville', ['up' => false]);
-//        }
-//
-//        $villes = $entityManager->getRepository(Ville::class)->findAll();
-//
-//        return $this->redirectToRoute('app_ville', ['up' => false]);
-//    }
-
-
-
-
-    #[Route('/admin/ville/{id<\d+>}/update', name: 'app_admin_ville_update')]
-    public function update(int $id, Request $request,
-                           VilleRepository $villeRepository,
-                           EntityManagerInterface $entityManager): Response
+    #[Route('/admin/ville/{id<\d+>}/update', name: 'ville_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Ville $ville, EntityManagerInterface $entityManager): Response
     {
-        $ville = $villeRepository->find($id);
         $form = $this->createForm(VilleType::class, $ville);
         $form->handleRequest($request);
+
         if ($form->isSubmitted() && $form->isValid()) {
-            // Persist
             $entityManager->flush();
-            //Add Succes notif
-            $this->addFlash('success', 'La sortie a été modifiée avec succès');
-            return $this->redirectToRoute('app_sorties_detail', ['id' => $ville->getId()]);
+
+            return $this->redirectToRoute('app_ville');
         }
 
-        $up = true;
-        return $this->redirectToRoute('app_ville');
+        return $this->render('ville/edit.html.twig', [
+            'ville' => $ville,
+            'form' => $form->createView(),
+        ]);
     }
 
-    #[Route('/admin/ville/{id<\d+>}/delete', name: 'app_admin_ville_delete')]
-    public function delete(int $id, VilleRepository $villeRepository, EntityManagerInterface $entityManager): Response
+    #[Route('/admin/ville/{id<\d+>}/delete', name: 'ville_delete', methods: ['POST'])]
+    public function delete(Request $request, Ville $ville, EntityManagerInterface $entityManager): Response
     {
-        $ville = $villeRepository->find($id);
-        if(!$ville){
-            throw $this->createNotFoundException('Ville not found');
+        if ($this->isCsrfTokenValid('delete' . $ville->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($ville);
+            $entityManager->flush();
         }
-        $entityManager->remove($ville);
-        $entityManager->flush();
 
         return $this->redirectToRoute('app_ville');
     }
