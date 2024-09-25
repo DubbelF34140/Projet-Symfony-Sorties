2a32c15997ecd9cfe1c49b0a02a0830664f06ad1
sbodin2022 <bodin.seb@laposte.net>
2024-09-25 10:21:17 +0200

    unique entity sur Ville

--- src/Entity/Ville.php
+++ src/Entity/Ville.php
@@ -6,8 +6,11 @@ use App\Repository\VilleRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: VilleRepository::class)]
+#[UniqueEntity(fields: ['nom'], message: 'Ce nom est déjà utilisé')]
+#[UniqueEntity(fields: ['codePostal'], message: 'Ce codePostal est déjà utilisé')]
 class Ville
 {
     #[ORM\Id]
